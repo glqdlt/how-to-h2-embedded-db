66d5b63a2a45245e2970af66fc24e904151e879c
jhun <jhun@nm-4ones.com>
2019-05-22 11:23:56 +0900

    구현

--- src/main/java/com/glqdlt/ex/howtoh2embeddeddatabase/HowToH2EmbeddedDatabaseApplication.java
+++ src/main/java/com/glqdlt/ex/howtoh2embeddeddatabase/HowToH2EmbeddedDatabaseApplication.java
@@ -1,13 +1,29 @@
 package com.glqdlt.ex.howtoh2embeddeddatabase;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class HowToH2EmbeddedDatabaseApplication {
+public class HowToH2EmbeddedDatabaseApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(HowToH2EmbeddedDatabaseApplication.class, args);
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        String url = dataSource.getUrl();
+        if (!url.equals("jdbc:h2:file:~/local/file_db;")) {
+            log.error("입력 된 URL {}", url);
+            throw new RuntimeException("잘못 로드 된 DataSource");
+        }
+    }
 }
--- src/test/java/com/glqdlt/ex/howtoh2embeddeddatabase/HowToH2EmbeddedDatabaseApplicationTests.java
+++ src/test/java/com/glqdlt/ex/howtoh2embeddeddatabase/HowToH2EmbeddedDatabaseApplicationTests.java
@@ -1,16 +1,31 @@
 package com.glqdlt.ex.howtoh2embeddeddatabase;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HowToH2EmbeddedDatabaseApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void getDataSource() {
+        Object dataSource = applicationContext.getBean("dataSource");
+        String url = ((DataSource) dataSource).getUrl();
+        Assert.assertEquals("jdbc:h2:mem:memory_db;", url);
+    }
 }
