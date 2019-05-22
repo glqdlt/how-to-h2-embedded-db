a73b16cb5b3ea1e1704883e6b41f6669b10ebb9e
jhun <jhun@nm-4ones.com>
2019-05-22 11:06:02 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/howtoh2embeddeddatabase/HowToH2EmbeddedDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.howtoh2embeddeddatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToH2EmbeddedDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToH2EmbeddedDatabaseApplication.class, args);
+    }
+
+}
